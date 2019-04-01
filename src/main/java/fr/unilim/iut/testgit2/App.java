a954d0711ddac6b547c1935ab016b5eb70853ade
valentin rameau <valentin.rameau@etu.unilim.fr>
2019-04-01 15:06:18 +0200

    commit initizl

---
+++ src/main/java/fr/unilim/iut/testgit2/App.java
@@ -0,0 +1,13 @@
+package fr.unilim.iut.testgit2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
